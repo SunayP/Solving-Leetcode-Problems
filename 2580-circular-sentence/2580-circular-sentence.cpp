b7fc81df929ff5c4b20ac417d9f55845fb9ff758
Sunay Pendela <157937147+SunayP@users.noreply.github.com>
2024-11-02 08:49:52 +0530

    Time: 0 ms (100%), Space: 8.2 MB (59.78%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        string str = "";
-        for(int i=0; i<sentence.size(); i++){
-            if(sentence[i] == ' '){
-                if(str.back() != sentence[i+1]){
-                    return false;
-                }
-                str = "";
-            }
-            else{
-                str += sentence[i];
-            }
-        }
         if(sentence.back() != sentence[0]){
             return false;
         }
+        for(int i=0; i<sentence.size(); i++){
+            if(sentence[i] == ' '){
+                if(sentence[i-1] != sentence[i+1]){
+                    return false;
+                }
+            }
+        }
+        
         return true;
     }
 };
